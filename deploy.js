b8891d6424ce554217d05a0447f508183de14ede
shell <470326964@qq.com>
2018-08-31 20:45:33 +0800

    fix: bug

--- deploy.js
+++ deploy.js
@@ -6,23 +6,26 @@ promisifyAll(fs);
 const blankList = ['README.md'];
 const regexp = /.+\.md$/;
 const gitUrl = 'https://github.com/AfterThreeYears/blog/blob/master';
-
-(async () => {
-  const filenames = await fs.readdirAsync('.');
-  const mds = filenames.filter(filename => {
-    return regexp.test(filename.toLowerCase()) && !blankList.includes(filename);
-  });
-
-  try {
-    let content = `# BLOG
+let content = `# BLOG
 https://github.com/AfterThreeYears/blog/issues
 \n`;
-    content += mds.map(mdFile => {
-      const mdUrl = `${gitUrl}/${encodeURIComponent(mdFile)}`;
-      return `## [${mdFile}](${mdUrl})`;
-    }).join('\n');
-    fs.writeFileAsync(resolve(__dirname, blankList[0]), content);
+
+(async () => {
+  let filenames;
+  try {
+    filenames = await fs.readdirAsync('.');
   } catch (error) {
-      
+    console.error(`[读取错误]: ${error.message}`);
+  }
+  const mds = filenames.filter(filename => regexp.test(filename.toLowerCase()) && !blankList.includes(filename));
+
+  content += mds.map(mdFile => {
+    const mdUrl = `${gitUrl}/${encodeURIComponent(mdFile)}`;
+    return `## [${mdFile}](${mdUrl})`;
+  }).join('\n');
+  try {
+    await fs.writeFileAsync(resolve(__dirname, blankList[0]), content);
+  } catch (error) {
+    console.error(`[写入错误]: ${error.message}`);
   }
 })();
